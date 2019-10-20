9bfe1aaad7af5376e9fef9c436de1878c113f7f0
simatei <dr.robsimatei@gmail.com>
2019-10-20 19:54:35 +0300

    add frontend directory

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+import CustomLayout from './containers/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
